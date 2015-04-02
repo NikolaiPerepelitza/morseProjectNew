ebd4c9200a3cfc1a8a8974860626e3ac00657666
NikolaiPerepelitza <nikolaip17@riverdale.k12.or.us>
2015-04-01 21:36:39 -0700

    Arduino program for Morse Project

---
+++ morse.ino
@@ -0,0 +1,31 @@
+#include <morse.h>
+
+Morse morse(13);
+
+void setup() {
+}
+
+void loop() {
+morse.D();
+morse.a();
+morse.m();
+morse.i(); 
+morse.a();
+morse.n();
+morse.i();
+morse.s();
+morse.t(); 
+morse.h(); 
+morse.e(); 
+morse.b(); 
+morse.e(); 
+morse.s(); 
+morse.t();
+/* By the way, 
+DAMIAN 
+IS 
+THE 
+BEST!!!!
+:)
+*/
+}
